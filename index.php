85cbbe463a61e0f7c86a20784d313fd099f67dd3
Fernando <fcgraciano@gmail.com>
2024-07-30 18:20:18 -0300

    Dia 30-07

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php include "cabecalho.php"; ?>
+
+<h1>Página Inicial do site </h1>
+
+<div class="alert alert-success" role="alert">
+  Cadastrado com sucesso
+</div>
+
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+
+
+<div class="card" style="width: 18rem;">
+  <img src="notebook.jpeg" class="card-img-top" alt="Notebook novo">
+  <div class="card-body">
+    <h5 class="card-title">Notebook Asus 16gb RAM Core I5</h5>
+    <p class="card-text">
+        R$ 2500,00
+    </p>
+    <a href="#" class="btn btn-primary">Comprar</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="Cadeira-gamer.jpg" class="card-img-top" alt="Notebook novo">
+  <div class="card-body">
+    <h5 class="card-title">Cadeira Gamer</h5>
+    <p class="card-text">
+        R$ 999,00
+    </p>
+    <a href="#" class="btn btn-primary">Comprar</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="playstation5.jpg" class="card-img-top" alt="Notebook novo">
+  <div class="card-body">
+    <h5 class="card-title">playstation 5</h5>
+    <p class="card-text">
+        R$ 5.000,00
+    </p>
+    <a href="#" class="btn btn-primary">Comprar</a>
+  </div>
+</div>
+
+<?php include "rodape.php"; ?>
