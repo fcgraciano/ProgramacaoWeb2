8efb242679020f21b30090f7ef44c94b61330185
Ferneko <fcgraciano@gmail.com>
2024-10-29 17:11:01 -0300

    Editar produto

--- editar_produto.php
+++ editar_produto.php
@@ -1,6 +1,30 @@
-<?php include "cabecalho.php"; ?>
+<?php include "cabecalho.php"; include "conexao.php";  ?>
+<?php 
+    if( isset($_POST["descricao"]) && !empty($_POST["descricao"]) &&
+        isset($_POST["valor"]) && !empty($_POST["valor"]) 
+      )
+    {
+        $descricao = $_POST['descricao'];
+        $valor = str_replace(",",".",$_POST['valor']) ;
+        $imagem = $_POST['imagem'];
+        $id = $_GET['id'];
+        $sql = "Update Produtos set Descricao = '$descricao', Valor = $valor, Imagem = '$imagem' where Id = $id";
+        $resultado = $conexao->query($sql);
+        if($resultado)
+        {
+            header("location: produtos.php");
+        }
+        else
+        {
+            echo "<div class='alert alert-danger'>";
+            echo  "Erro ao salvar os dados";
+            echo "</div>";
+        }
+    }
+?>
+
 <?php
-    include "conexao.php"; 
+    
     $sql = "SELECT * from produtos where id = $_GET[id]";
     $resultado = $conexao->query($sql);
     while ($row = $resultado->fetch_assoc()) 
@@ -11,9 +35,7 @@
     }
 
 ?>
-<form action="editar_produto.php"
-         method="post"
-          enctype="multipart/form-data">
+<form action="editar_produto.php?id=<?php echo $_GET['id']; ?>" method="post" enctype="multipart/form-data">
     <label>Descrição do Produto</label>
     <input class="form-control"
              value="<?php echo $descricao; ?>" 
@@ -25,9 +47,10 @@
             name="valor" />
     <br>
   
-    <input type="file" name="imagem" accept="image/*" required> 
+    <input type="file" name="novaImagem" accept="image/*"> 
+    <input type="hidden" name="imagem" value="<?php echo $imagem; ?>"/> 
     <br>
-    <img src="<?php echo "/".basename(__DIR__). "/img/$imagem" ; ?>" />
+    <img src="<?php echo "/".basename(__DIR__). "/img/$imagem" ; ?>" width="150" height="100" />
     <br>
     <button type="submit" class="btn btn-success">
         Enviar dados
